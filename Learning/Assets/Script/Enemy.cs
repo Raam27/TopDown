c912231429eea3c3186e159f3bfaffc521aa8f2b
Raam27 <45362796+Raam27@users.noreply.github.com>
2020-07-14 03:32:40 +0800

    First
    Basic
    -Player Move
    -Light
    -Enemy

---
+++ Learning/Assets/Script/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D r2d;
+    public float distance;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            distance = Vector2.Distance(player.transform.position, gameObject.transform.position);
+            move();
+            if (distance > 6)
+            {
+                player = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject;
+        }
+    }
+
+    private void rotate()
+    {
+        Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+        transform.up = direction;
+    }
+    private void move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,player.transform.position,speed*Time.deltaTime);
+        rotate();
+    }
+}
---
+++ Learning/Assets/Script/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Movement : MonoBehaviour
+{
+    
+    public int health = 100;
+    [SerializeField]
+    int rotationSpeed;
+    private Rigidbody2D r2d;
+    public int movSpeed;
+    float jumpForce;
+
+    float horizontalInput;
+    float verticalInput;
+    float rotateInput;
+    Vector3 worldPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        rotatePlayer();
+    }
+    private void FixedUpdate()
+    {
+        movePlayer();
+        
+    }
+    private void movePlayer()
+    {
+        r2d.velocity = new Vector2(horizontalInput*movSpeed, verticalInput*movSpeed);
+    }
+    private void rotatePlayer()
+    {
+        //float rotation = worldPosition* rotationSpeed;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
